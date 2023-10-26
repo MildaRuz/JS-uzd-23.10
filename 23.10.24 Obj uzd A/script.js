8a06920a7e74840013b23abaaf45db6b53375a03
milda_ruzgiene <milda.marcinkute@gmail.com>
2023-10-26 09:03:54 +0300

    troleibusas

--- 23.10.24 Obj uzd A/script.js
+++ 23.10.24 Obj uzd A/script.js
@@ -33,3 +33,36 @@ pinigine1.ideti(13);
 pinigine1.ideti(1);
 
 pinigine1.skaiciuoti();
+
+//----- 3 uzd ------
+//Sukurti klasę Troleibusas. Konstruktoriuje sukurti savybę keleiviuSkaicius
+//kuri yra lygi 0. Parašyti du metodus: ilipa(keleiviuSkaicius) ir
+//islipa(keleiviuSkaicius). O taip pat parašyti metoda vaziuoja(),
+//kuris į konsolę išvestų troleibusu važiuojančių keleivių skaičių. Atkreipkite dėmesį,
+//kad troleibusu važiuoti neigiamas keleivių skaičius negali.
+class Troleibusas {
+  constructor() {
+    this.keleiviuSkaicius = 0;
+  }
+  ilipa(keleiviuSkaicius) {
+    this.keleiviuSkaicius += keleiviuSkaicius;
+  }
+  islipa(keleiviuSkaicius) {
+    if (this.keleiviuSkaicius < keleiviuSkaicius) {
+      this.keleiviuSkaicius = 0;
+    } else this.keleiviuSkaicius -= keleiviuSkaicius;
+  }
+  vaziuoja() {
+    console.log(`Troleibusu važiuoja ${this.keleiviuSkaicius} keleivių`);
+  }
+}
+
+const troleibusas11 = new Troleibusas();
+
+troleibusas11.ilipa(10);
+troleibusas11.islipa(2);
+troleibusas11.ilipa(3);
+
+troleibusas11.vaziuoja();
+
+//----- 4 uzd ------
---
+++ 23.10.26 uzd A DOM/script.js
@@ -0,0 +1,16 @@
+//----- 1 uzd ------
+//Atskiri elementai
+//a)Tamsiai žaliai nuspalvinti h1 tagą;
+const h1 = document.getElementsByTagName('h1');
+h1.style.color = 'green';
+
+//b)Tagui i pridėti klasę small;
+
+//c)Iš tago h1 pašalinti klasę main;
+
+//d)Tik tam h2 tagui, kuris iškart eina po h1 tago, pridėti klasę first
+//ir pašalinti klasę main;
+//https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/remove
+
+//e) Pirmam span tagui, kuris yra h2 viduje sumažinti fonto dydį iki 10px
+//ir nudažyti pilkai
